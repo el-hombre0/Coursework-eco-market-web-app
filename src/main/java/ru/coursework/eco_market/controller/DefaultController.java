53d31c8fb3b6674b494a1cd50ee9e870652e056b
Stanislav <evendot@yandex.ru>
2022-05-10 02:19:14 +0300

    Added admin panel to create, edit and delete product's types and products

--- src/main/java/ru/coursework/eco_market/controller/DefaultController.java
+++ src/main/java/ru/coursework/eco_market/controller/DefaultController.java
@@ -3,8 +3,7 @@ package ru.coursework.eco_market.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.coursework.eco_market.entity.Product;
 import ru.coursework.eco_market.entity.ProductType;
 import ru.coursework.eco_market.repository.ProductRepository;
@@ -38,5 +37,75 @@ public class DefaultController {
         Product product = productRepository.findById(id).orElse(null);
         model.addAttribute("product", product);
         return "product";
+    }
+
+    @GetMapping("/productTypeList")
+    public String productTypeList(Model model) {
+        Iterable<ProductType> types = productTypeRepository.findAll();
+        model.addAttribute("types", types);
+        return "productTypeList";
+    }
+
+    @GetMapping("productTypeList/add")
+    public String productTypeListAdd(Model model) {
+        ProductType productType = new ProductType();
+        model.addAttribute("productType", productType);
+        return "productTypeForm";
+    }
+    @PostMapping("productTypeList/add")
+    public String productTypeListAddSubmit(@ModelAttribute ProductType productType, Model model){
+        productTypeRepository.save(productType);
+        model.addAttribute("types", productTypeRepository.findAll());
+        return "productTypeList";
+    }
+
+    @GetMapping("/productTypeList/delete/{productTypeId}")
+    public String productTypeListDelete(@PathVariable("productTypeId") long id, Model model) {
+        productTypeRepository.deleteById(id);
+        model.addAttribute("types", productTypeRepository.findAll());
+        return "productTypeList";
+    }
+
+    @GetMapping("/productTypeList/edit/{productTypeId}")
+    public String productTypeListEdit(@PathVariable("productTypeId") long id, Model model) {
+        ProductType productType = productTypeRepository.findById(id).orElse(null);
+        model.addAttribute("productType", productType);
+        return "productTypeForm";
+    }
+
+    ////////////////////////////
+    @GetMapping("/productList")
+    public String productList(Model model) {
+        Iterable<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "productList";
+    }
+
+    @GetMapping("productList/add")
+    public String productListAdd(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "productForm";
+    }
+
+    @PostMapping("productList/add")
+    public String productListAddSubmit(@ModelAttribute Product product, Model model){
+        productRepository.save(product);
+        model.addAttribute("product", productRepository.findAll());
+        return "productList";
+    }
+
+    @GetMapping("/productList/delete/{productId}")
+    public String productListDelete(@PathVariable("productId") long id, Model model) {
+        productRepository.deleteById(id);
+        model.addAttribute("products", productRepository.findAll());
+        return "productList";
+    }
+
+    @GetMapping("/productList/edit/{productId}")
+    public String productListEdit(@PathVariable("productId") long id, Model model) {
+        Product product = productRepository.findById(id).orElse(null);
+        model.addAttribute("product", product);
+        return "productForm";
     }
 }
