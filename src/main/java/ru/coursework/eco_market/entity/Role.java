bf09ce11230d8977da587ed0b8fdfd451dff9ec1
Stanislav <evendot@yandex.ru>
2022-05-20 03:26:42 +0300

    Fixed bugs

--- src/main/java/ru/coursework/eco_market/entity/Role.java
+++ src/main/java/ru/coursework/eco_market/entity/Role.java
@@ -1,5 +1,11 @@
 package ru.coursework.eco_market.entity;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER, ADMIN;
+    @Override
+    public String getAuthority(){
+        return name();
+    }
 }
